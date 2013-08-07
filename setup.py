a1dadd7458f8daf6baedd6f54198ebe7e7f1b61c
Trey Tabner <trey.tabner@rackspace.com>
2013-08-06 22:52:18 -0500

    Update setup.py and add copyright

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@ from setuptools import setup
 
 setup(
     name='rest-shell',
+    version='1.0',
+    author = 'Trey Tabner',
+    author_email = 'trey@tabner.com',
+    url = 'https://github.com/treytabner/rest-shell',
     packages=['rest_shell'],
     entry_points={
         'console_scripts': [
             'rest-shell = rest_shell:main',
         ],
     },
-    version='1.0',
     install_requires=[
         'bottle',
         'requests',
